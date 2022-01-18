670cd0866b8a87c87440a1b797bb09601ed18384
Talha Bin Ashraf <talhabinashraf@gmail.com>
2022-01-19 02:27:37 +0600

    Selenium installed Project Structure Updated

---
+++ ecommerce/src/test/java/com/automationpractice/ecommerce/testCases/FirstTc.java
@@ -0,0 +1,21 @@
+package com.automationpractice.ecommerce.testCases;
+
+
+import com.automationpractice.ecommerce.pages.First;
+import org.testng.annotations.Test;
+
+public class FirstTc extends BaseClass {
+
+    @Test
+    public void firstTc(){
+
+        First f =new First(driver);
+
+        f.signInButton.click();
+        System.out.println("Test ran Properly  ");
+
+
+    }
+
+
+}
